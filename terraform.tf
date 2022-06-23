259746164176d6e412498e598fde588779d2b8bd
Travis <tnewton1@users.noreply.github.com>
2022-06-23 11:37:10 -0400

    add terraform file

---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source = "Telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+provider "proxmox" {
+  # Configuration options
+}
+
